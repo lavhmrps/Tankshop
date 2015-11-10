e2acc436960444ac3af24708907317ba1b033024
Magnus Lunde <maglunde@gmail.com>
2015-11-10 17:23:31 +0100

    first commit

---
+++ DAL/Customer/ICustomerRepo.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using Nettbutikk.Model;
+
+namespace DAL.Customer
+{
+    public interface ICustomerRepo
+    {
+        bool DeleteCustomer(string email);
+        List<CustomerModel> GetAllCustomers();
+        CustomerModel GetCustomer(int customerId);
+        CustomerModel GetCustomer(string email);
+    }
+}
